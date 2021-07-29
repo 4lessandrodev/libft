e0825a2ba0de47253b1799390585c9b1f7926deb
4lessandrodev <alessandroadm@live.com>
2021-07-29 20:40:46 -0300

    feat: added ft_isdigit

--- libft.h
+++ libft.h
@@ -1,6 +1,13 @@
 /**
- * @brief this function receives a int and if it refere to an alpha
- * @param c int reference to char ascii code
- * @return int 1 if is alpha else 0
+ * @brief this function receives an int value and check if it is an alphabet letter based on ascii code
+ * @param c int
+ * @return int 1 if is alphabet else 0
 */
 int	ft_isalpha(int c);
+
+/**
+ * @brief this function receives an int and check if it is a number based on ascii code
+ * @param c int
+ * @return int 1 if is alpha else 0
+*/
+int	ft_isdigit(int c);
