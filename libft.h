c84775a97b85a93fe38bd2acbe5a40295907827e
4lessandrodev <alessandroadm@live.com>
2021-07-29 21:54:32 -0300

    feat: added isascii function

--- libft.h
+++ libft.h
@@ -8,6 +8,20 @@ int	ft_isalpha(int c);
 /**
  * @brief this function receives an int and check if it is a number based on ascii code
  * @param c int
- * @return int 1 if is alpha else 0
+ * @return int 1 if is digit else 0
 */
 int	ft_isdigit(int c);
+
+/**
+ * @brief this function receives an int and check if it is alphanumeric based on ascii code
+ * @param c int
+ * @return int 1 if is alphanumeric else 0
+*/
+int	ft_isalnum(int c);
+
+/**
+ * @brief this function receives an int and check if it is ascii 0 to 120
+ * @param c int
+ * @return int 1 if is ascii else 0
+*/
+int	ft_isascii(int c);
